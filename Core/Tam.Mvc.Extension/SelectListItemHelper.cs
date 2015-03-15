260c40a2492fed985a5266d66a5fd32eb8434479
gitbulk <giacatiensinh@hotmail.com>
2015-03-15 19:49:56 +0700

    add class
    add class SelectListItemHelper

---
+++ Core/Tam.Mvc.Extension/SelectListItemHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Tam.Mvc.Extension
+{
+    public static class SelectListItemHelper
+    {
+        public static SelectListItem EmptySelectList(string text, string value = "")
+        {
+            return new SelectListItem
+            {
+                Text = text,
+                Value = value
+            };
+        }
+    }
+}
