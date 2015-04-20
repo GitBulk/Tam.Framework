249e3411896b257f1bb241648db174f24000d1a5
gitbulk <giacatiensinh@hotmail.com>
2015-04-21 00:18:15 +0700

    update
    expand partial class GreatBlogEntities

---
+++ Blog/Tam.Blog.Model/GreatBlogEntities.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace Tam.Blog.Model
+{
+    public partial class GreatBlogEntities : DbContext
+    {
+        /// <summary>
+        /// Constructor GreatBlogEntities
+        /// </summary>
+        /// <param name="connectionString">Entity Framework connection string</param>
+        public GreatBlogEntities(string connectionString)
+            : base(connectionString)
+        {
+        }
+    }
+
+}
