8f452d5837f96369d88c7422267cae088aeafb08
gitbulk <giacatiensinh@hotmail.com>
2016-07-20 15:08:21 +0700

    Add form upload

---
+++ Core/Tam.WebApi.Extension/Configuration/WebApiUtil.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Tam.WebApi.Extension.Configuration
+{
+    public static class WebApiUtil
+    {
+        public static void EnableCamelCasePropertyNames(this HttpConfiguration config)
+        {
+            var formatters = config.Formatters;
+            var jsonFormatter = formatters.JsonFormatter;
+            var settings = jsonFormatter.SerializerSettings;
+            settings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
