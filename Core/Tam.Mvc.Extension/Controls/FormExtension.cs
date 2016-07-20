5b9f40bdd17f72b7e7278e07508c59dd5dd11f30
gitbulk <giacatiensinh@hotmail.com>
2016-07-20 15:09:02 +0700

    add form upload file

---
+++ Core/Tam.Mvc.Extension/Controls/FormExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Tam.Mvc.Extension.Controls
+{
+    public static class FormExtension
+    {
+        public static MvcForm BeginFormUpload(this HtmlHelper helper, string actionName, string controllerName)
+        {
+            return helper.BeginForm(actionName, controllerName, FormMethod.Post, new { enctype = "multipart/form-data" });
+        }
+    }
+}
