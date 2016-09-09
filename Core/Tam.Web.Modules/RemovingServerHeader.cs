b7faa55327fd44d0e69947290ce8d57c7f8c1540
gitbulk <giacatiensinh@hotmail.com>
2016-09-09 15:32:49 +0700

    add modules

---
+++ Core/Tam.Web.Modules/RemovingServerHeader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web;
+
+namespace Tam.Web.Modules
+{
+    public class RemovingServerHeader: IHttpModule
+    {
+        // http://r2d2.cc/2011/10/21/how-to-remove-server-x-aspnet-version-x-aspnetmvc-version-and-x-powered-by-from-the-response-header-in-iis7/
+        public void Dispose()
+        {
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.PreSendRequestHeaders += OnPreSendRequestHeaders;
+        }
+
+        private void OnPreSendRequestHeaders(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.Headers.Remove("Server");
+        }
+    }
+}
