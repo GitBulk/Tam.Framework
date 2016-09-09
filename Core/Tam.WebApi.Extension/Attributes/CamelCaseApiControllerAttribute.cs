ad9911115d9f882e21b0b813a793bfaa69c80383
gitbulk <giacatiensinh@hotmail.com>
2016-09-09 15:33:13 +0700

    add filters for web api

---
+++ Core/Tam.WebApi.Extension/Attributes/CamelCaseApiControllerAttribute.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http.Controllers;
+
+namespace Tam.WebApi.Extension.Attributes
+{
+    public class CamelCaseApiControllerAttribute : Attribute, IControllerConfiguration
+    {
+        public void Initialize(HttpControllerSettings controllerSettings, HttpControllerDescriptor controllerDescriptor)
+        {
+            var jsonMediaTypeFormatter = controllerSettings.Formatters.OfType<JsonMediaTypeFormatter>().SingleOrDefault();
+            if (jsonMediaTypeFormatter != null)
+            {
+                controllerSettings.Formatters.Remove(jsonMediaTypeFormatter);
+                jsonMediaTypeFormatter = new JsonMediaTypeFormatter
+                {
+                    SerializerSettings =
+                    {
+                        ContractResolver = new CamelCasePropertyNamesContractResolver()
+                    }
+                };
+                controllerSettings.Formatters.Add(jsonMediaTypeFormatter);
+            }
+        }
+    }
+
+    //[CamelCaseApiController]
+    //public class SomeController : ApiController
+    //{
+    //...
+    //}
+}
