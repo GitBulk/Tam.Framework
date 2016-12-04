54fe737886e3dc53efaf5d7dfc80040ecb8dfeda
gitbulk <giacatiensinh@hotmail.com>
2016-12-04 18:10:43 +0700

    add a supporting project for .Net Core

---
+++ NetCore/Tam.NetCore.Util/MvcBuilderHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+
+namespace Tam.NetCore.Util
+{
+    public static class MvcBuilderHelper
+    {
+        public static void AddMvcCamelCasePropertyNames(this IMvcBuilder builder)
+        {
+            builder.AddJsonOptions(o => o.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver());
+        }
+    }
+}
