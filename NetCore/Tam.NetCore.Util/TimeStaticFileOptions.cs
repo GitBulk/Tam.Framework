c0220a6e4791af0b1e6dab4ac740bea1bfa00364
gitbulk <giacatiensinh@hotmail.com>
2016-12-04 18:30:02 +0700

    add a supporting class for StaticFileOptions

---
+++ NetCore/Tam.NetCore.Util/TimeStaticFileOptions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Net.Http.Headers;
+using System;
+
+namespace Tam.NetCore.Util
+{
+    /// <summary>
+    /// Usage: app.UseStaticFiles(new TimeStaticFileOptions(a number)
+    /// </summary>
+    public class TimeStaticFileOptions : StaticFileOptions
+    {
+        public TimeStaticFileOptions(int durationInSeconds = 60) : base()
+        {
+            if (durationInSeconds < 1)
+            {
+                throw new Exception("Dutaion cannot less than one second.");
+            }
+            this.OnPrepareResponse = ctx =>
+            {
+                ctx.Context.Response.Headers[HeaderNames.CacheControl] = "public,max-age=" + durationInSeconds;
+            };
+        }
+    }
+}
